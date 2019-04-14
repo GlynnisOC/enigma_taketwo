a5472c46713f37d376112f867dd63316d9694c73
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-04-14 14:36:31 -0600

    Add key test to always return 5 digits

--- enigma/test/key_test.rb
+++ enigma/test/key_test.rb
@@ -8,4 +8,10 @@ class KeyTest < Minitest::Test
     key = Key.new
     assert_instance_of Key, key
   end
-end       
+
+  def test_a_key_will_return_5_digits
+    key = Key.new
+    key.supply_digits
+    assert_equal 5, key.supply_digits.to_s.size
+  end
+end
