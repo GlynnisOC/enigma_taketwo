8e780e1bfab0f4dec0b6c40437d6c298bfbd9153
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-04-15 14:41:16 -0600

    Add hash of keys method. Tested as well to ensure datatype returned is correct

--- enigma/test/shift_test.rb
+++ enigma/test/shift_test.rb
@@ -12,7 +12,15 @@ class ShiftTest < Minitest::Test
   end
 
   def test_it_can_add_key_and_date_digits_together
+    skip
     shift = Shift.new
-    assert_equal [79, 45, 11, 58], shift.add_date_and_key_digits_together
+    assert_equal [79, 45, 11, 58], shift.add_date_and_key_digits
+  end
+
+  def test_it_returns_a_hash_of_keys
+    shift = Shift.new
+    shift.add_date_and_key_digits
+    expected = {:A=> 79, :B=> 45, :C=> 11, :D=> 58}
+    assert_equal expected, shift.hash_of_keys
   end
 end
