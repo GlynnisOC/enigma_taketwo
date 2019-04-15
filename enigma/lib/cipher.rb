15cfcc866d792bf2bfb4bc54cdbb740c9740c7a3
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-04-15 17:39:01 -0600

    Add test to divide message into 4 sub arrays of ind. strings for each char

--- enigma/lib/cipher.rb
+++ enigma/lib/cipher.rb
@@ -1,6 +1,15 @@
 class Cipher
+  attr_reader :message
 
   def initialize(message)
-    @messsage = message
+    @message = message
+  end
+
+  def div_message_into_fours
+    four_characters = []
+    @message.chars.each_slice(4) do |chars|
+      four_characters.push(chars)
+    end
+    four_characters
   end
 end
--- enigma/test/cipher_test.rb
+++ enigma/test/cipher_test.rb
@@ -11,4 +11,14 @@ class CipherTest < Minitest::Test
     cipher = Cipher.new('hey there')
     assert_instance_of Cipher, cipher
   end
+
+  def test_it_is_init_with_message
+    cipher = Cipher.new('hey there')
+    assert_equal 'hey there', cipher.message
+  end
+
+  def test_it_divides_message_into_four_blocks
+    cipher = Cipher.new('hey there')
+    assert_equal [["h", "e", "y", " "], ["t", "h", "e", "r"], ["e"]], cipher.div_message_into_fours
+  end
 end
