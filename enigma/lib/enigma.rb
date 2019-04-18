cd6e503819f8e25e0c6521356bf769efce247d89
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-04-17 18:41:52 -0600

    Decrypt working properly and CLI behaving as it should

--- enigma/lib/enigma.rb
+++ enigma/lib/enigma.rb
@@ -4,12 +4,12 @@ require './lib/cipher'
 class Enigma
   include Creator
 
-  def encrypt(message, key = create_key, date = create_date)
+  def encrypt(message, key=create_key, date=create_date)
     cipher = Cipher.encrypt(message, key, date)
     { encryption: cipher, key: key, date: date }
   end
 
-  def decrypt(message, key = create_key, date = create_date)
+  def decrypt(message, key=create_key, date=create_date)
     cipher = Cipher.decrypt(message, key, date)
     { encryption: cipher, key: key, date: date }
   end
--- enigma/test/shift_test.rb
+++ enigma/test/shift_test.rb
@@ -7,18 +7,15 @@ class ShiftTest < Minitest::Test
   # end
   #
   # def test_it_returns_a_hash_of_keys
-  #   skip
   #   expected = {:A=> 79, :B=> 45, :C=> 11, :D=> 58}
   #   assert_equal expected, shift.hash_of_keys
   # end
   #
   # def test_it_shifts_letters_with_hash_of_keys
-  #   skip
   #   assert_equal "l", shift_letters_with_hash_of_keys("h", 4)
   # end
   #
   # def test_it_can_shift_four_letters_properly
-  #   skip
   #   assert_equal ['b', 'p', 'c', 'y'], shift.shift_message_in_fours(["y", "e", "l", "l"], [1, 2, 3, 4])
   # end
 end
